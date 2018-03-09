0fdaaaf641d3defe709bc6017d443729b74e32bd
fzheng <fzheng@thoughtworks.com>
2018-03-09 16:28:58 +0800

    init server

---
+++ src/routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  // res.render({ title: 'Express' });
+  res.send({status: 'running'});
+});
+
+module.exports = router;
