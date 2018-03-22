7cb163b3f5876df5e3fd8532bf3c69960aae7cf9
fzheng <fzheng@thoughtworks.com>
2018-03-22 09:50:52 +0800

    add fetching user opend id info api

--- src/routes/wechatMiniApp.js
+++ src/routes/wechatMiniApp.js
@@ -1,11 +1,22 @@
-const express = require('express');
+import  express from 'express';
+import axios from 'axios';
+
+import {appId, appSecret} from '../config';
+
 const router = express.Router();
 
-const { getSession } = require('../services/wechatUsers');
-
-
 router.get('/get-session',  function(req, res, next) {
+  const userCodeOfMiniApp = req.query.code;
+  const url = `https://api.weixin.qq.com/sns/jscode2session?appid=${appId}&secret=${appSecret}&js_code=${userCodeOfMiniApp}&grant_type=authorization_code`
+  axios.get(url).then(response => {
+    console.log(response.data);
+    return reponse;
+  }).catch(error => {
+    return error;
+  })
   res.send({status: '1'});
 });
+
+
 
 module.exports = router;
