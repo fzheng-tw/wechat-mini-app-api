e1f7f729775cd8c3a38c0078efad7b3c103b83fe
fzheng <fzheng@thoughtworks.com>
2018-03-22 10:04:33 +0800

    change get user openId api to post api to pass code through body of req

--- src/routes/wechatMiniApp.js
+++ src/routes/wechatMiniApp.js
@@ -5,8 +5,8 @@ import {appId, appSecret} from '../config';
 
 const router = express.Router();
 
-router.get('/get-session',  function(req, res, next) {
-  const userCodeOfMiniApp = req.query.code;
+router.post('/get-session',  function(req, res, next) {
+  const userCodeOfMiniApp = req.body.code;
   const url = `https://api.weixin.qq.com/sns/jscode2session?appid=${appId}&secret=${appSecret}&js_code=${userCodeOfMiniApp}&grant_type=authorization_code`
   axios.get(url).then(response => {
     console.log(response.data);
